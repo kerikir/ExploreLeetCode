5cb2a596ab184a69eb82ad496a7ad98bbc4aa912
kerikir <vlasov-kerik@mail.ru>
2026-01-31 21:35:38 +0300

    added template code to FizzBuzz.ky

---
+++ src/main/kotlin/com/leetcode/kerikir/beginner/FizzBuzz.kt
@@ -0,0 +1,28 @@
+package com.leetcode.kerikir.beginner
+
+
+/**
+ * Тема: Условное выражение
+ *
+ * Задание: Сформировать строку исходя из входного числа
+ * Ограничения: 1 <= N <= 10_000
+ *
+ * Время =  мс ( %)
+ * Память =  МБ ( %)
+ *
+ * Временная сложность = O()
+ * Пространственная сложность = O()
+ */
+class Solution5 {
+    fun fizzBuzz(n: Int): List<String> {
+
+    }
+}
+
+
+
+fun main() {
+    val n = 3
+    val result = Solution5().fizzBuzz(n)
+    println(result)
+}

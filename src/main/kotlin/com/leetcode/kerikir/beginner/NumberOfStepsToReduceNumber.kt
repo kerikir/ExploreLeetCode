0b618eb6de604a1d8adbafd0a436418d9f493291
kerikir <vlasov-kerik@mail.ru>
2026-01-31 22:02:30 +0300

    template code added to NumberOfStepsToReduceNumber.kt

---
+++ src/main/kotlin/com/leetcode/kerikir/beginner/NumberOfStepsToReduceNumber.kt
@@ -0,0 +1,29 @@
+package com.leetcode.kerikir.beginner
+
+
+/**
+ * Тема: Четность числа
+ *       Побитовый операции
+ *
+ * Задание: Количество шагов для уменьшения числа до нуля
+ * Ограничения: 0 <= N <= 1_000_000
+ *
+ * Время =  мс ( %)
+ * Память =  МБ ( %)
+ *
+ * Временная сложность = O()
+ * Пространственная сложность = O()
+ */
+class Solution6 {
+    fun numberOfSteps(num: Int): Int {
+
+    }
+}
+
+
+
+fun main() {
+    val n = 14
+    val result = Solution6().numberOfSteps(n)
+    println(result)
+}

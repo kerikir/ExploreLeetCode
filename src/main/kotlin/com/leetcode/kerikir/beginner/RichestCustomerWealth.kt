40198f4be79648bab962994f44b86e2bdcf89a69
kerikir <vlasov-kerik@mail.ru>
2026-01-31 03:35:02 +0300

    added the RichestCustomerWealth.kt

---
+++ src/main/kotlin/com/leetcode/kerikir/beginner/RichestCustomerWealth.kt
@@ -0,0 +1,30 @@
+package com.leetcode.kerikir.beginner
+
+
+/**
+ * Тема: Итерация по двумерному массиву
+ *
+ * Задание: Определить строку с наибольшей суммой
+ * Ограничения: 1 <= M <= 50
+ *              1 <= N <= 50
+ *              1 <= K <= 100
+ *
+ * Время =  мс ( %)
+ * Память =  МБ ( %)
+ *
+ * Временная сложность = O()
+ * Пространственная сложность = O()
+ */
+class Solution3 {
+    fun maximumWealth(accounts: Array<IntArray>): Int {
+        return 0
+    }
+}
+
+
+
+fun main() {
+    val array = arrayOf(intArrayOf(1,5), intArrayOf(7,3), intArrayOf(3,5))
+    val result = Solution3().maximumWealth(array)
+    println(result)
+}

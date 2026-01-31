b15ddab10bffb50fd0d7203c73129e0d32a6d473
kerikir <vlasov-kerik@mail.ru>
2026-01-31 22:28:16 +0300

    added a solution for Number of Steps to Reduce a Number to Zero with bitwise operations

--- src/main/kotlin/com/leetcode/kerikir/beginner/NumberOfStepsToReduceNumberBitwiseOperations.kt
+++ src/main/kotlin/com/leetcode/kerikir/beginner/NumberOfStepsToReduceNumberBitwiseOperations.kt
@@ -19,16 +19,22 @@ class Solution7 {
         var number = num
         var isEven = false
 
+        // Число для уменьшения на 1 (сброс последней цифры в 0)
+        val value = 0b11111111111111111110
+
         var numberOfSteps = 0
 
         while (number > 0) {
-            isEven = (number % 2) == 0
+            // Проверка четности числа - побитовое умножение на 1 (проверка последней цифры)
+            isEven = (number and 1) == 0
 
             if (isEven) {
-                number /= 2
+                // Деление числа на 2 - сдвиг битов вправо на 1
+                number = number shr 1
 
             } else {
-                number--
+                // Вычитание 1 - сброс последней цифры в 0
+                number = number and value
             }
 
             numberOfSteps++
@@ -41,7 +47,7 @@ class Solution7 {
 
 
 fun main() {
-    val n = 14
+    val n = 123
     val result = Solution7().numberOfSteps(n)
     println(result)
 }
